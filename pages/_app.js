603786ccf241aba70c6637e9a9df0cc5b7cc031b
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-04-12 08:53:23 +0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/globals.css';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer';
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default MyApp
