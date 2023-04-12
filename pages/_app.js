1ec51a98ff7f89422af5faeff2f23551a256d249
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-04-12 09:01:19 +0300

    change bootstrap path

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer';
