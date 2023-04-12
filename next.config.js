818465bf856fc01da7866759aef3023c40831da8
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-04-12 10:07:15 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
   compress: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
