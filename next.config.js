d4d78dc605068a1dbde7baa7b45313a3ca97df12
DmitryPonomarenko34 <dimaponomarenko443@gmail.com>
2023-04-12 09:33:58 +0300

    fix error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  compress: false
+  compress: false,
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../styles/globals.css';
-import Header from '../components/Header.jsx';
-import Footer from '../components/Footer';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,7 +12,7 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Footer />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
